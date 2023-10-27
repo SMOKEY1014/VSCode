d382541bc638eb0a290cd0e0f976331f51920361
SMOKEY1014 <smokeydhlamini@gmail.com>
2023-10-27 11:21:08 +0200

    Webs

---
+++ GymFIT-website/script.js
@@ -0,0 +1,16 @@
+
+
+
+var menu = document.getElementById("bar");
+var item = document.getElementById("item");
+
+item.style.right = '-300px';
+menu.onclick = function () {
+    /*body...*/
+if (item.style.right == '-300px') {
+    item.style.right = '0';
+}
+else{
+    item.style.right = '-300px';
+}  
+}
